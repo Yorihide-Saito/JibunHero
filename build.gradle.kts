218113ecbea37d4f6728d450aeefacaed6b0a1d2
Yorihide-Saito <yorihide.saito@gmail.com>
2025-03-18 22:00:36 +0900

    dokka と spotless の追加
    - dokka: Kdoc用のプラグイン
    - spotless: Lintツール

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    // トップレベルの適応しないため apply false
+    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.spotless) apply false
 }
